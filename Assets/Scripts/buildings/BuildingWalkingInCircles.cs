6ccb0af28886006fd7f9def939395cc7c74412df
Jonas Schartau Grooss <jonasgrooss@hotmail.com>
2021-11-10 16:01:54 +0100

    Jeremy has been created
    Jeremy is a "building" that walks in circles and damages enemies directly in front of him.
    
    Jeremy can die.

---
+++ Assets/Scripts/buildings/BuildingWalkingInCircles.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingWalkingInCircles : MonoBehaviour, IActor
+{
+    double StartPos;
+    public float speed;
+    private float rotation = 0.1f;
+    public float damage;
+    public CollisionObserver detectionCollision;
+    public CollisionObserver damagerCollision;
+
+    public float maxHealth = 100;
+    public Image healthbar;
+
+    private float currentHealth;
+
+    public ActorType type => ActorType.Obstacle;
+    public float Health { get => currentHealth; 
+            set
+            {
+                currentHealth = value;
+                StartCoroutine(SmoothSliderDecrease(currentHealth / maxHealth, healthbar));
+                if (currentHealth <= 0f)
+                {
+                    Die();
+                }
+            }
+        }
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position.x;
+        detectionCollision.Subscribe(Detection_Stay, CollisionObserver.CollisionType.Stay);
+        detectionCollision.Subscribe(Detection_Exit, CollisionObserver.CollisionType.Exit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+        transform.Rotate(0.0f, rotation, 0.0f, Space.Self);
+    }
+
+    private void Detection_Stay(Collider other)
+    {
+        IActor actor = other.GetComponent<IActor>();
+        if (actor == null)
+            return;
+
+        switch (actor.type)
+        {
+            case ActorType.Enemy:
+                actor.Health -= damage;
+                Debug.Log("Damage dealt to enemy");
+                break;
+        }
+    }
+
+    private void Detection_Exit(Collider other)
+    {
+        IActor actor = other.GetComponent<IActor>();
+        if (actor == null)
+            return;
+    }
+
+        private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator SmoothSliderDecrease(float amount, Image image)
+    {
+        if (image != null)
+            while (image.fillAmount > amount)
+        {
+            if(image != null)
+            image.fillAmount -= 0.01f;
+
+            yield return new WaitForSeconds(0.000001f);
+        }
+    }
+}
