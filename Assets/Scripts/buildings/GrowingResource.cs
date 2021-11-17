ed38189cd8140427e1b8b7624480e108ac296e2c
Jonas Schartau Grooss <jonasgrooss@hotmail.com>
2021-11-17 19:03:18 +0100

    Created plantable resource
    The plantable resource growns during waves, until it reaches a maximum size at which point a timer counts until the plant is automatically harvested
    
    Currently can't be placed because of an unknown bug.

---
+++ Assets/Scripts/buildings/GrowingResource.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingResource : MonoBehaviour, IActor
+{
+    private float timer;
+    private float growthTime = 2.0f;
+    private float harvestTimer = 5.0f;
+    private int growthStage;
+    private int maxResourcesGained = 5;
+    private int currentResourcesGained;
+    public float health;
+
+    Vector3 growth = new Vector3(0.1f,0.1f,0.1f);
+    public ActorType type => ActorType.Obstacle;
+    public float Speed { get => Speed; set => Speed = 0; }
+    public float MaxHealth => 100;
+    public float Health { get => health; set => health = value; }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.instance.inBattle == true)
+        {
+            timer += Time.deltaTime;
+            checkHealth();
+            checkGrowth();
+            checkFullyGrown();
+        }
+    }
+
+    void checkGrowth()
+    {
+        if(timer > growthTime && growthStage < 10)
+        {
+            growthStage++;
+            this.gameObject.transform.GetChild(0).transform.localScale += growth;
+            timer = 0;
+        }
+    }
+
+    void checkFullyGrown()
+    {
+        if(growthStage == 10 && timer > harvestTimer)
+        {
+            calculateResourcesGained();
+            GameManager.instance.resource += currentResourcesGained;
+            Destroy(this.gameObject);
+        }
+    }
+
+    void harvestManual()
+    {
+        if(growthStage == 1);
+    }
+
+    void calculateResourcesGained()
+    {
+        float percentageLifeRemaining = (health/MaxHealth);
+        int resourceValue = (int)(maxResourcesGained * percentageLifeRemaining);
+        currentResourcesGained = resourceValue;
+    }
+
+    void checkHealth()
+    {
+        if(Health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
