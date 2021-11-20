9d6b97fb76eb6576b02ddc39421029e83e1d2b10
Jesper Rabjerg <lxc355@alumni.ku.dk>
2021-11-20 15:46:05 +0100

    Working on interactable ui. Has a bug.

--- Assets/Scripts/InteractableUI.cs
+++ Assets/Scripts/InteractableUI.cs
@@ -3,45 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System;
 
 public class InteractableUI : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    [Flags]
+    public enum MouseClickEvent
+    {
+        None = 0,
+        LeftMouseButton = 1,
+        RightMouseButton = 2,
+        MiddleMouseButton = 4
+    }
+
     // Events that other classes can subscribe to
     public delegate void MouseEvent(GameObject interactedUI, PointerEventData eventData);
     public MouseEvent OnEnter;
     public MouseEvent OnExit;
     public MouseEvent OnClicked;
 
-    [Header("References")]
-    public GameObject mainGameObject;
-    public GameObject highlightUI;
-    public Image icon;
+    [Header("Options")]
+    [Tooltip("If enabled, then the highligh image will replace the default image.")]
+    public bool replaceWithHighlight;
+    [Tooltip("If enabled, then the toggle image will replace the default image and highlight image.")]
+    public bool replaceWithToggledImage;
+    [Tooltip("If this is a button, specify the mouse buttons to use.")]
+    public MouseClickEvent mouseButton;
 
-    private Color initialIconColor;
+    [Header("References")]
+    [Tooltip("The main/parent GameObject of a UI element.")]
+    public GameObject mainGameObject;
+    public Image image;
+    public Image highlightImage;
+    public Image toggledImage;
+
+    private Color initialImageColor;
     private bool inside = false;
 
+    // Use this for player controllers.
+    public static bool UIInteraction = false;
+
     private bool interactable = true;
+    private bool toggled = false;
+
     public bool Interactable { get => interactable; 
         set {
             if (value)
             {
-                
+                if (image != null)
+                {
+                    image.color = initialImageColor;
+                }
             } else
             {
-                if (highlightUI != null)
-                    highlightUI.SetActive(false);
+                SetHighlighted(inside);
 
-                if (icon != null)
+                if (image != null)
                 {
-                    icon.color = new Color(icon.color.r, icon.color.g, icon.color.b, 0.5f);
+                    image.color = new Color(image.color.r / 2, image.color.g / 2, image.color.b / 2, image.color.a / 2);
                 }
             }
         } 
     }
 
+    public bool Toggled
+    {
+        get => toggled;
+
+        set
+        {
+            if (toggledImage == null)
+                throw new Exception("There is no toggled image to toggle.");
+
+            // TODO: finish this
+        }
+    }
+
     private void Start()
     {
+        if (image != null)
+            initialImageColor = image.color;
 
+        if (mainGameObject == null)
+            throw new Exception("Cannot have an interactable UI with no main GameObject.");
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -58,8 +102,7 @@ public class InteractableUI : MonoBehaviour, IPointerClickHandler, IPointerEnter
         inside = true;
         if (Interactable)
         {
-            if (highlightUI != null)
-                highlightUI.SetActive(true);
+            SetHighlighted(inside);
 
             if (OnEnter != null)
                 OnEnter(mainGameObject, eventData);
@@ -71,11 +114,22 @@ public class InteractableUI : MonoBehaviour, IPointerClickHandler, IPointerEnter
         inside = false;
         if (Interactable)
         {
-            if (highlightUI != null)
-                highlightUI.SetActive(false);
+            SetHighlighted(inside);
 
             if (OnExit != null)
                 OnExit(mainGameObject, eventData);
         }
+    }
+
+    private void SetHighlighted(bool highlight)
+    {
+        // Sanitise input
+        if (highlightImage == null)
+            return;
+
+        highlightImage.gameObject.SetActive(highlight);
+
+        if (replaceWithHighlight)
+            image.gameObject.SetActive(!highlight);
     }
 }
