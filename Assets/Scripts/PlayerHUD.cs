dbdbd4a50075f12446d0b70a6946445016ba0174
Jonas Schartau Grooss <jonasgrooss@hotmail.com>
2021-11-23 10:57:41 +0100

    Made damage indicators for player and base health
    When the player or the base takes damage, their respective healthbar will quickly blink red, indicating that damage has been taken

--- Assets/Scripts/PlayerHUD.cs
+++ Assets/Scripts/PlayerHUD.cs
@@ -12,20 +12,55 @@ public class PlayerHUD : MonoBehaviour
     public Base baseController;
     public Slider playerHealthBar;
     public Slider baseHealthBar;
+    public Image baseHealthFill;
+    public Image playerHealthFill;
     public TextMeshProUGUI resourceCounter;
+
+    private float oldBaseHealth;
+    private float oldPlayerHealth;
 
     private void Start()
     {
         playerController = PlayerManager.instance.playerController;
         gameManager = GameManager.instance;
+        oldPlayerHealth = playerController.Health;
+        oldBaseHealth = baseController.Health;
     }
 
     private void Update()
     {
+        
         // Update player values on the hud
+        if(oldPlayerHealth > playerController.Health)
+        {
+            StartCoroutine(playerHealthBarDamageIndicator());
+        }
         playerHealthBar.value = playerController.Health / playerController.MaxHealth;
+        if(oldBaseHealth > baseController.Health)
+        {
+            StartCoroutine(baseHealthBarDamageIndicator());
+        }
         baseHealthBar.value = baseController.Health / baseController.MaxHealth;
 
         resourceCounter.text = gameManager.resource.ToString();
+
+        oldBaseHealth = baseController.Health;
+        oldPlayerHealth = playerController.Health;
+    }
+
+
+    
+    IEnumerator baseHealthBarDamageIndicator()
+    {
+        baseHealthFill.color = new Color32(255,0,0,255);
+        yield return new WaitForSeconds(0.05f);
+        baseHealthFill.color = new Color32(133,160,39,255);
+    }
+
+    IEnumerator playerHealthBarDamageIndicator()
+    {
+        playerHealthFill.color = new Color32(255,0,0,255);
+        yield return new WaitForSeconds(0.05f);
+        playerHealthFill.color = new Color32(133,160,39,255);
     }
 }
