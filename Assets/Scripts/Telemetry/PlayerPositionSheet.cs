69ff5a85f22d59e4f305d55cc3aadb8767cdea9b
Jonas Schartau Grooss <jonasgrooss@hotmail.com>
2022-04-19 15:47:17 +0200

    PlayerPositionSheet

---
+++ Assets/Scripts/Telemetry/PlayerPositionSheet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPositionSheet
+{
+    public int playerID;
+    public int gameSessionID;
+    public int levelSessionID;
+    public int levelID;
+    public float xPosition;
+    public float zPosition;
+
+//https://docs.google.com/forms/d/e/1FAIpQLScjiaT6Dyj2rTSn1ebm16b1N_TqBjqEVa26YViD3hNBC_S0fA/viewform?usp=sf_link
+    public const string url = "https://docs.google.com/forms/d/e/1FAIpQLScjiaT6Dyj2rTSn1ebm16b1N_TqBjqEVa26YViD3hNBC_S0fA/";
+    public const string url_viewform = "https://docs.google.com/forms/d/e/1FAIpQLScjiaT6Dyj2rTSn1ebm16b1N_TqBjqEVa26YViD3hNBC_S0fA/viewform?usp=sf_link";
+    public const string form_playerID = "entry.860392843";
+    public const string form_gameSessionID= "entry.761608052";
+    public const string form_levelSessionID= "entry.1377904516";
+    public const string form_levelID= "entry.2012788902";
+    public const string form_xPosition= "entry.70625257";
+    public const string form_zPosition= "entry.1617084394";
+}
